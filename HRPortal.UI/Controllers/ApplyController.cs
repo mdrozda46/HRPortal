80a1d526971956e4adbb8d09f6ac09c28e24124a
Mike Drozda <mdrozda@gmail.com>
2015-10-23 16:26:29 -0400

    Started add new resume functionality

--- HRPortal.UI/Controllers/ApplyController.cs
+++ HRPortal.UI/Controllers/ApplyController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRPortal.Data;
+using HRPortal.Models;
 
 namespace HRPortal.UI.Controllers
 {
@@ -10,6 +12,26 @@ namespace HRPortal.UI.Controllers
     {
         // GET: Apply
         public ActionResult Index()
+        {
+            return View(new Resume());
+        }
+
+        [HttpPost]
+        public ActionResult AddResume(Resume resume)
+        {
+
+            
+            var repo = Factory.CreateResumeRepository();
+
+            resume.DateOfApplication = DateTime.Now;
+            var resumes = repo.GetAll();
+            resume.ID = resumes.Max(r => r.ID) + 1;
+            repo.Add(resume);
+
+            return View("Result");
+        }
+
+        public ActionResult AddEducation(Resume resume)
         {
             return View();
         }
