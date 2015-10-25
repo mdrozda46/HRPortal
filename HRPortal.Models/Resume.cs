b450377e81b3d75ca2a7ba5c53c5f0de0da79a19
Chary Gurney <chary.gurney@gmail.com>
2015-10-25 11:42:13 -0400

    succesfully adding job histories and employment histories

--- HRPortal.Models/Resume.cs
+++ HRPortal.Models/Resume.cs
@@ -9,6 +9,12 @@ namespace HRPortal.Models
 {
     public class Resume
     {
+        public Resume()
+        {
+            EducationHistory = new List<Education> {new Education(), new Education(), new Education()};
+            EmploymentHistory = new List<Employment> {new Employment(), new Employment(), new Employment()};
+        }
+
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -16,9 +22,9 @@ namespace HRPortal.Models
         public string PhoneNumber { get; set; }
         public string Position { get; set; }
         public List<Employment> EmploymentHistory { get; set; }
-        public Employment AddedEmploymentHistory { get; set; }
+        //public Employment AddedEmploymentHistory { get; set; }
         public List<Education> EducationHistory { get; set; }
-        public Education AddedEducationHistory { get; set; }
+        //public Education AddedEducationHistory { get; set; }
 
         [DataType(DataType.Currency)]
         public decimal DesiredSalary { get; set; }
