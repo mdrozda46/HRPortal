3266c5816c8abdabc4a199bed6f5dffbefb04872
Chary Gurney <chary.gurney@gmail.com>
2015-11-01 17:53:31 -0500

    beautified some of the pages and fixed drop down issue

--- HRPortal.Models/Education.cs
+++ HRPortal.Models/Education.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace HRPortal.Models
     {
         public string Name { get; set; }
         public string Type { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
     }
 }
--- HRPortal.Models/Policy.cs
+++ HRPortal.Models/Policy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace HRPortal.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public string Category { get; set; }
     }
